e57c48456084ecca52771edbf64180f14ad7e3fb
DenverJensen <jensendenver@gmail.com>
2022-12-03 22:14:07 -0700

    home page framework

--- pokesite/pokepacks/views.py
+++ pokesite/pokepacks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from re import template
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+from django.template import loader
+
 # Create your views here.
+
+
+def home(request):
+    context = {
+    }
+    return render(request, 'pokepacks/home.html', context)
