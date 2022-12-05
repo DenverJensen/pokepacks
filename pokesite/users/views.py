96451875d6b406a8ab6668dc84396af576165562
DenverJensen <89622716+DenverJensen@users.noreply.github.com>
2022-12-04 22:44:05 -0700

    login formatting

--- pokesite/users/views.py
+++ pokesite/users/views.py
@@ -4,19 +4,28 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 # Create your views here.
 
-
 def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, 'Welcome '+username+'! please login')
-        return redirect('/login')
+		form = UserCreationForm()
+				
+		if request.method == "POST":
 
+				form = UserCreationForm(request.POST)
 
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'form':form})
+				if form.is_valid():
+						user = form.save(commit=False)
+
+						user.is_valid = False
+						user.save()
+						username = form.cleaned_data.get('username')
+						messages.success(request, 'Welcome '+username+'! please login')
+						return redirect('/login')
+				else:
+						messages.warning(request, 'Invalid registration details')
+						
+		return render(
+				request, "users/register.html",
+				{"form": form}
+		)
 
 def profile (request):
     return render(request, 'users/profile.html')
